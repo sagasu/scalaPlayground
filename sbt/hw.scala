a5204d3f14cc578bcb302a51253ff7416d8c9e0e
sagasu <sagasu@gmail.com>
2016-01-09 20:08:36 +0000

    checking out various sbt functionalities.

--- sbt/hw.scala
+++ sbt/hw.scala
@@ -1,3 +1,6 @@
 object Hi {
-  def main(args: Array[String]) = println("Hi!")
+  def main(args: Array[String]) = println("Hi!" + factorial(30))
+
+  def factorial(x:BigInt): BigInt =
+    if(x == 0) 1 else x * factorial(x-1)
 }
