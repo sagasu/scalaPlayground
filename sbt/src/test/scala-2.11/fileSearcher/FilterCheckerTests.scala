d212fb8072ec331b7836026bf036e7fb88177a47
sagasu <sagasu@gmail.com>
2016-02-12 22:24:51 +0000

    applying even more infix notations

--- sbt/src/test/scala-2.11/fileSearcher/FilterCheckerTests.scala
+++ sbt/src/test/scala-2.11/fileSearcher/FilterCheckerTests.scala
@@ -7,14 +7,14 @@ class FilterCheckerTests extends FlatSpec {
   "return a list with that file" in {
     val matchingFile = new FileObject("match")
     val listOfFiles = List(new FileObject("random"), matchingFile)
-    val matchedFiles = FilterChecker("match").findMatchedFiles(listOfFiles)
+    val matchedFiles = FilterChecker("match") findMatchedFiles listOfFiles
     assert(matchedFiles == List(matchingFile))
   }
   
   "Filter checker passed a list with a directory that matches the filter" should
   "should not return the dictionary" in {
     val listOfIOObjects = List(new FileObject("random"), new DirectoryObject("match"))
-    val matchedFiles = FilterChecker("match").findMatchedFiles(listOfIOObjects)
+    val matchedFiles = FilterChecker("match") findMatchedFiles listOfIOObjects
     assert(matchedFiles.length == 0)
   }
 }
