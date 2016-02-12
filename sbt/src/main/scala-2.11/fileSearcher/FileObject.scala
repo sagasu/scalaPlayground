2aab75d9eb841a90e2f0cb431f0d6acfd2685194
sagasu <sagasu@gmail.com>
2016-02-12 22:22:32 +0000

    removing unneeded parentheses and using infix notation for content.contains(filter) => content contains filter.

--- sbt/src/main/scala-2.11/fileSearcher/FileObject.scala
+++ sbt/src/main/scala-2.11/fileSearcher/FileObject.scala
@@ -4,5 +4,5 @@ trait IOObject{
   val name: String
 }
 
-class FileObject(val name: String) extends IOObject{}
-class DirectoryObject(val name: String) extends IOObject {}
+class FileObject(val name: String) extends IOObject
+class DirectoryObject(val name: String) extends IOObject
