46f7612d09a10966f7ad7792a872e1b94a09fc7c
sagasu <sagasu@gmail.com>
2016-02-13 17:40:51 +0000

    adding factorial implementation w/o tail recursion

--- sbt/src/main/scala-2.11/playground/FactorialPlayground.scala
+++ sbt/src/main/scala-2.11/playground/FactorialPlayground.scala
@@ -1,6 +1,10 @@
 package playground
 
 class FactorialPlayground {
+  def factNoTailRecursion(n:Int): Int = 
+    if(n == 0) 1
+    else n * factNoTailRecursion(n-1) 
+  
   def fact(n: Int) = factHelper(n,1)
   
   def factHelper(n:Int, acc: Int):Int = {
--- sbt/src/test/scala-2.11/playground/FactorialPlaygroundTests.scala
+++ sbt/src/test/scala-2.11/playground/FactorialPlaygroundTests.scala
@@ -3,7 +3,11 @@ package playground
 import org.scalatest.FunSuite
 
 class FactorialPlaygroundTests extends FunSuite {
-  test("Factorial of 3 should return 6"){
+  test("Factorial of method with TailRecursion 3 should return 6"){
     assert(new FactorialPlayground().fact(3) == 6)
+  }
+  
+  test("Factorial of 3 should return 6"){
+    assert(new FactorialPlayground().factNoTailRecursion(3) == 6)
   }
 }
